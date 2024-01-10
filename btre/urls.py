5ac963322503cdbe4670c9b34a449724cd513e46
Favour <georgefavour694@gmail.com>
2024-01-10 14:24:24 +0100

    custom page titles

--- btre/urls.py
+++ btre/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('accounts/', include('accounts.urls')),
     path('listings/', include('listings.urls')),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls)
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, state_choices
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings,
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.all()
+    context = {
+        'realtors': realtors
+    }
+    return render(request, 'pages/about.html', context)
